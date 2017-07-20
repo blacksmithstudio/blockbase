8876a0af08b100a013b69313396c021ed022073f
Alexandre Pereira <alexandrepereira@alexandsmacbook.home>
2017-07-20 10:56:23 +0200

    adding dynamic drivers support

--- src/drivers.js
+++ src/drivers.js
@@ -1,16 +1,22 @@
+const _ = require('underscore')
 const fs = require('fs')
-const npm = require('npm')
 const junk = require('junk')
+const path = require('path')
 
 module.exports = (app) => {
     return {
         async build(path = `${app.root}/drivers`) {
-            npm.load(function(err, npm) {
-                npm.commands.ls([], true, (err, data, lite) => {
-                    // for(let k in data.dependencies)
-                    //     console.log(k)
+            if(fs.existsSync(`${app.root}/../node_modules/@blacksmithstudio`)){
+                let modules = fs.readdirSync(`${app.root}/../node_modules/@blacksmithstudio`)
+                let drivers = _.filter(modules, (mod) => {
+                    return mod.includes('blockbase-')
                 })
-            })
+
+                drivers.forEach((driver, idx) => {
+                    if(!app.drivers[driver.replace('blockbase-', '')])
+                        app.drivers[driver.replace('blockbase-', '')] = require(`@blacksmithstudio/${driver}`)(app)
+                })
+            }
 
             if(fs.existsSync(path)){
                 let files = fs.readdirSync(path).filter(junk.not)
